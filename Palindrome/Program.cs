4659cbbaaa0ddc70bdc1e2e51b26c064cf088d92
Anela Moćević <anelamocevic@gmail.com>
2022-02-21 12:15:20 +0100

    Reversing entered string and chack if it is palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Palindrome
+{
+	class Program
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Enter some string");
+			string enteredString = Console.ReadLine();
+			
+			char[] enteredStringToChar = enteredString.ToCharArray();
+			Array.Reverse(enteredStringToChar);
+			string reversedString = new string(enteredStringToChar);
+			bool isPalindrome = false;
+			if(enteredString == reversedString)
+            {
+				isPalindrome = true;
+            }
+            Console.WriteLine(isPalindrome);
+		}
+	}
+}
